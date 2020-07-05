9eaa13ea635eb8bfebbef7d26d0e8e17b1cb971d
Matěj Jehlička <jehlicka@mjehlicka.eu>
2020-07-05 19:03:55 +0200

    Add RF24 checker implementation

--- src/RF24Checker.cpp
+++ src/RF24Checker.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "RF24Checker.h"
+
+RF24Checker *RF24Checker::init(int CS, int CE) {
+    RF24Checker *rf24Checker = new RF24Checker(CS, CE);
+    return rf24Checker;
+}
+
+bool RF24Checker::check() {
+    return false;
+}
+
+long RF24Checker::getAddress() {
+    return this->radio.getTXAddr();
+}
--- src/RF24Checker.h
+++ src/RF24Checker.h
@@ -5,9 +5,44 @@
 #ifndef FAKE_WALKIE_TALKIE_RF24CHECKER_H
 #define FAKE_WALKIE_TALKIE_RF24CHECKER_H
 
+//#include "printf.h"
+#include "SPI.h"
+#include "RF24.h"
 
 class RF24Checker {
+public:
+    /**
+     * Initialize new instance of checker.
+     *
+     * @return new instance of checker
+     * */
+    static RF24Checker *init(int CS, int CE);
 
+    /**
+     * Check if RF24 is connected correctly. Return TRUE if device
+     * MAC is not 0x00 otherwise return FALSE.
+     *
+     * @return true - device is connected correctly
+     * @return false - device is connected incorrectly
+     * */
+    bool check();
+
+private:
+    /**
+     * Default constructor initialize CS and CE pin.
+     * */
+    RF24Checker(int CS, int CE) : CS(CS), CE(CE), radio(CS, CE) {
+        radio.begin();
+    }
+
+public:
+    long getAddress();
+
+private:
+    long address;
+    const int CS;
+    const int CE;
+    RF24 radio;
 };
 
 
