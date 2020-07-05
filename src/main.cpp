9a81ccd0dec2ad13e275e4c30e5e684801f4c4af
Matěj Jehlička <jehlicka@mjehlicka.eu>
2020-07-05 19:03:20 +0200

    Add address checking

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 #include "AudioController.h"
+#include "RF24Checker.h"
 
 AudioController *audioController;
+RF24Checker *rf24Checker;
 
 void setup(){
     Serial.begin(9600);
-
+    Serial.println("Fake walkie-talkie");
+    rf24Checker = RF24Checker::init(RF_CS, RF_CE);
     audioController = AudioController::init();
     audioController->openFile(FILE_NAME);
+    Serial.println(rf24Checker->getAddress(), HEX);
 }
 
 void loop(){
-    audioController->play();
+    if(rf24Checker->check()){
+        audioController->play();
+    }
 }
