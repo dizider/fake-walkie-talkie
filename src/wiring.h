77fcd78f3075cd3de46a7445730051c35f5ff43e
Matěj Jehlička <jehlicka@mjehlicka.eu>
2020-07-09 00:04:49 +0200

    update/ wiring - pins

--- src/wiring.h
+++ src/wiring.h
@@ -7,17 +7,14 @@
 
 #include <Arduino.h>
 
-const int SD_CS = 4; //SD card's CS pin
+const int SD_CS = 4;
 const int SPEAKER_PIN = 9;
-const int VOLUME = 6;
+const int VOLUME = 7;
 
 const int RF_CS = 2;
 const int RF_CE = 3;
 
-//Playback file name
-const char FILE_NAME[] = "example.wav";
-
-const int redLedPin = 1;
+const int redLedPin = 7;
 const int greenLedPin = 1;
 const int blueLedPin = 1;
 
